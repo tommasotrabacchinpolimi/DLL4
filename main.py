e95cf324827a72b7e5803704ef98a8b4504702b4
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2025-01-11 14:57:10 +0100

    fixed mask size bug

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ class LastCustomDecoderLayer(nn.TransformerDecoderLayer):
             x,
             memory,
             memory,
-            attn_mask=memory_mask,
+            attn_mask=memory_mask[:32,:,:],
             key_padding_mask=None,
             is_causal=False,
             need_weights=True,
